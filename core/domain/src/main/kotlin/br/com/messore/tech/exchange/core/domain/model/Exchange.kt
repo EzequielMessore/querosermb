b4b4b29d504f7f10f35806d832fb1048ecafdca9
Ezequiel Messore <ezequiel.messore@hotmail.com>
2024-12-12 08:48:29 -0300

    feat(domain): Mapeando objeto de exchange para o dominio

---
+++ core/domain/src/main/kotlin/br/com/messore/tech/exchange/core/domain/model/Exchange.kt
@@ -0,0 +1,18 @@
+package br.com.messore.tech.exchange.core.domain.model
+
+import java.text.NumberFormat
+import java.util.Locale
+
+data class Exchange(
+    val name: String,
+    val website: String,
+    val exchangeId: String,
+    val volume1DayUsd: Double,
+    val image: String? = null,
+) {
+    val formattedVolume: String
+        get() {
+            val numberFormat = NumberFormat.getCurrencyInstance(Locale.US)
+            return numberFormat.format(volume1DayUsd)
+        }
+}
