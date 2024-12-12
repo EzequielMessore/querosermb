6e80e5524bc16ec7000a954c96e84397a81d8bfc
Ezequiel Messore <ezequiel.messore@hotmail.com>
2024-12-12 08:45:57 -0300

    feat(core:testing): Criando modulo auxiliar de tests

---
+++ core/testing/src/main/java/br/com/messore/tech/exchanges/testing/ExchangeDataFactory.kt
@@ -0,0 +1,24 @@
+package br.com.messore.tech.exchanges.testing
+
+import br.com.messore.tech.exchange.core.domain.model.Exchange
+
+object ExchangeDataFactory {
+    fun createExchangeList(): List<Exchange> {
+        return listOf(
+            Exchange(
+                exchangeId = "BINANCE",
+                website = "https://www.binance.com/",
+                name = "Binance",
+                volume1DayUsd = 29497214160.58,
+                image = "https://s3.eu-central-1.amazonaws.com/bbxt-static-icons/type-id/png_64/12706e78b23e41e2afd42eecc28d9bbc.png",
+            ),
+            Exchange(
+                exchangeId = "KRAKEN",
+                website = "https://www.kraken.com/",
+                name = "Kraken",
+                volume1DayUsd = 1966886113.43,
+                image = "https://s3.eu-central-1.amazonaws.com/bbxt-static-icons/type-id/png_64/0fce391a2d564c089735f4e42e0d8d79.png",
+            ),
+        )
+    }
+}
