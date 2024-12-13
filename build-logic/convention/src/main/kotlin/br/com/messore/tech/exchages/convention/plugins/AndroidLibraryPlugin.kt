64bd5c02748aec705ba78d4b860655b4f27a85d0
Ezequiel Messore <ezequiel.messore@hotmail.com>
2024-12-12 21:35:41 -0300

    refactor(build-logic): Ajustando configuração do AndroidLibraryPlugin

--- build-logic/convention/src/main/kotlin/br/com/messore/tech/exchages/convention/plugins/AndroidLibraryPlugin.kt
+++ build-logic/convention/src/main/kotlin/br/com/messore/tech/exchages/convention/plugins/AndroidLibraryPlugin.kt
@@ -1,12 +1,15 @@
 package br.com.messore.tech.exchages.convention.plugins
 
 import br.com.messore.tech.exchages.convention.configs.configureKotlinAndroid
+import br.com.messore.tech.exchages.convention.extentions.bundle
+import br.com.messore.tech.exchages.convention.extentions.libs
 import com.android.build.api.dsl.LibraryExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.kotlin
+import org.gradle.kotlin.dsl.project
 
 class AndroidLibraryPlugin : Plugin<Project> {
     override fun apply(target: Project) = target.run {
@@ -27,6 +30,8 @@ class AndroidLibraryPlugin : Plugin<Project> {
 
         dependencies {
             "testImplementation"(kotlin("test"))
+            "testImplementation"(project(":core:testing"))
+            "testImplementation"(libs.bundle("testing"))
         }
     }
 }
