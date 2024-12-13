f8e68354e444c64a62d22cd92f9ad8c673d0cd39
Ezequiel Messore <ezequiel.messore@hotmail.com>
2024-12-12 21:33:18 -0300

    feat(core:testing): Reorganizando pacotes

--- core/testing/src/main/java/br/com/messore/tech/exchanges/testing/ExchangeDataFactory.kt
+++ core/testing/src/main/java/br/com/messore/tech/exchanges/testing/ExchangeDataFactory.kt
@@ -1,6 +1,7 @@
 package br.com.messore.tech.exchanges.testing
 
-import br.com.messore.tech.exchange.core.domain.model.Exchange
+import br.com.messore.tech.exchanges.core.domain.model.Exchange
+import br.com.messore.tech.exchanges.core.domain.model.Image
 
 object ExchangeDataFactory {
     fun createExchangeList(): List<Exchange> {
@@ -18,6 +19,19 @@ object ExchangeDataFactory {
                 name = "Kraken",
                 volume1DayUsd = 1966886113.43,
                 image = "https://s3.eu-central-1.amazonaws.com/bbxt-static-icons/type-id/png_64/0fce391a2d564c089735f4e42e0d8d79.png",
+            ),
+        )
+    }
+
+    fun createImageList(): List<Image> {
+        return listOf(
+            Image(
+                url = "https://www.binance.com/",
+                exchangeId = "BINANCE",
+            ),
+            Image(
+                url = "https://www.kraken.com/",
+                exchangeId = "KRAKEN",
             ),
         )
     }
