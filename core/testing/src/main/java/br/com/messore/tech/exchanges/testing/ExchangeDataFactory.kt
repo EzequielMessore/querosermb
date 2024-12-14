3091cc1978e74ba2c360889c88854acf5669958b
Ezequiel Messore <ezequiel.messore@hotmail.com>
2024-12-14 09:54:31 -0300

    feat(testing): Adicionar factory de tests de Exchanges

--- core/testing/src/main/java/br/com/messore/tech/exchanges/testing/ExchangeDataFactory.kt
+++ core/testing/src/main/java/br/com/messore/tech/exchanges/testing/ExchangeDataFactory.kt
@@ -10,18 +10,25 @@ object ExchangeDataFactory {
                 exchangeId = "BINANCE",
                 website = "https://www.binance.com/",
                 name = "Binance",
-                volume1DayUsd = 29497214160.58,
+                volume1DayUsd = 128855794.37,
+                volume1HourUsd = 29497214160.58,
+                volume1MonthUsd = 1234297742912.71,
                 image = "https://s3.eu-central-1.amazonaws.com/bbxt-static-icons/type-id/png_64/12706e78b23e41e2afd42eecc28d9bbc.png",
             ),
             Exchange(
                 exchangeId = "KRAKEN",
                 website = "https://www.kraken.com/",
                 name = "Kraken",
-                volume1DayUsd = 1966886113.43,
+                volume1DayUsd = 7611640.34,
+                volume1HourUsd = 1966886113.43,
+                volume1MonthUsd = 75269293477.50,
                 image = "https://s3.eu-central-1.amazonaws.com/bbxt-static-icons/type-id/png_64/0fce391a2d564c089735f4e42e0d8d79.png",
             ),
         )
     }
+
+    fun createExchangeById(exchangeId: String) = createExchangeList().find { it.exchangeId == exchangeId }
+        ?: throw IllegalArgumentException("Exchange not found")
 
     fun createImageList(): List<Image> {
         return listOf(
